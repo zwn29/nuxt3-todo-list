6ead8b46c68e20c0ce92bbbe681b0af0736c8848
zenf <78023279+zencodee@users.noreply.github.com>
2022-06-20 22:06:44 +0530

    first commit

---
+++ components/Todo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="text-pink-500 text-center border mx-1 mt-10 rounded py-2 flex justify-evenly "
+  >
+    <div class="">
+      {{ text }}
+    </div>
+    <div class="text-lg  ">
+      <span
+        @click="buttonClick"
+        class="cursor-pointer px-2  rounded m-1 border"
+      >
+        -
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['text'])
+const emit = defineEmits(['deleteItem'])
+
+const buttonClick = () => {
+  emit('deleteItem')
+}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class=" text-lg   text-pink-500 text-center border mx-1 mt-10 rounded py-2 "
+  >
+    Todo List
+  </div>
+  <form @submit.prevent>
+    <input
+      @keyup.enter="addTodo"
+      type="text"
+      v-model="newTodo"
+      placeholder="hello"
+      class="border box text-pink-200 focus:ring-transparent active:ring-transparent py-2 rounded block mx-1 mt-5 w-[98.8%] focus:outline-none "
+    />
+  </form>
+
+  <div v-for="a in arr">
+    <Todo :text="a" @delete-item="delItem" />
+  </div>
+</template>
+
+<script setup>
+const arr = ref([])
+const newTodo = ref('')
+
+const addTodo = e => {
+  if (newTodo.value !== '') {
+    arr.value.push(newTodo.value)
+    newTodo.value = ''
+    console.log(arr.value)
+  }
+}
+
+const delItem = a => {
+  console.log(a)
+  arr.value.splice(a, 1)
+}
+</script>
+
+<style>
+body {
+  background-color: #000;
+}
+
+.box {
+  border: 1px solid #ffe6e6 !important;
+}
+
+.box:focus {
+  border: 1px solid #f6c6ea !important;
+}
+</style>
